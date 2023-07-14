689417441ffa4e674cfe26e5238483e33c09d581
Amohelang Setseta <georgesetseta@gmail.com>
2023-07-14 11:54:23 -0700

    negative / positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,18 +16,17 @@ srand(time(NULL));
 /* Generate a random number between -100 and 100 */
 n = rand() % 201 - 100;
 
-printf("The number %d is ", n);
 if (n > 0)
 {
-printf("positive\n");
+printf("%d is positive\n", n);
 }
 else if (n == 0)
 {
-printf("zero\n");
+printf("%d is zero\n", n);
 }
 else
 {
-printf("negative\n");
+printf("%d is negative\n", n);
 }
 
 return (0);
