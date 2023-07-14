81f55187cfab5805e60a998d0bae72075ac20570
Amohelang Setseta <georgesetseta@gmail.com>
2023-07-13 17:28:33 -0700

    hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A Program thats uses puts function to print a line
+ *Return:0 (succesfu)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
