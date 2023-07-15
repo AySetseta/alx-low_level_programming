705bc8f6876f6014cb8cf82d743635f623cda69c
Amohelang Setseta <georgesetseta@gmail.com>
2023-07-15 13:05:57 -0700

    1-last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,10 +16,13 @@ int lastDigit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-lastDigit = abs(n) % 10;
+lastDigit = n % 10;
 {
 printf("Last digit of %d is %d ", n, lastDigit);
 }
+if (lastDigit < 0)
+lastDigit = -lastDigit;
+
 if (lastDigit > 5)
 {
 printf("and is greater than 5\n");
