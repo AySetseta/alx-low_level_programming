70da7dcd323f9b249f0422eff1623d63227682bf
Amohelang Setseta <georgesetseta@gmail.com>
2023-07-15 15:16:26 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+/* Loop through single-digit numbers from 0 to 9 */
+
+putchar('0');
+putchar('1');
+putchar('2');
+putchar('3');
+putchar('4');
+putchar('5');
+putchar('6');
+putchar('7');
+putchar('8');
+putchar('9');
+
+/* Print a new line */
+putchar('\n');
+
+return (0);
+}
