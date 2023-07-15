009c9a711ab4cecfc6af6e6d5859b873250b0585
Amohelang Setseta <georgesetseta@gmail.com>
2023-07-15 15:34:54 -0700

    print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+char c;
+
+/* print lower case alph */
+for (c = 'z'; c <= 'a'; c++)
+putchar(c);
+
+putchar('\n');
+
+return (0);
+}
