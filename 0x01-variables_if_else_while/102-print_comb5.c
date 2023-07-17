3222b5ebeb636e6ad5178d4033d52ff5032026ae
Amohelang Setseta <georgesetseta@gmail.com>
2023-07-17 04:20:48 -0700

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+int num1, num2;
+for (num1 = 0; num1 <= 99; num1++)
+{
+for (num2 = num1; num2 <= 99; num2++)
+{
+int tens1 = num1 / 10;
+int units1 = num1 % 10;
+int tens2 = num2 / 10;
+int units2 = num2 % 10;
+
+putchar('0' + tens1);
+putchar('0' + units1);
+putchar(' ');
+putchar('0' + tens2);
+putchar('0' + units2);
+
+if (num1 != 99 || num2 != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+
+putchar('\n');
+return (0);
+}
