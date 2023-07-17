cf319a131ff5de394ef31f410b848305c26e6829
Amohelang Setseta <georgesetseta@gmail.com>
2023-07-17 03:21:38 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,7 +17,7 @@ for (int k = j + 1; k <= 9; k++)
 /* Check if the combination has distinct digits*/
 putchar(i + '0');
 putchar(j + '0');
-putchar(j + '0');
+putchar(k + '0');
 if (i != 7 || j != 8 || k != 9)
 {
 putchar(',');
