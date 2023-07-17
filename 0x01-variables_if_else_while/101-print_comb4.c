682b7c07720d02f6074406f37f15048b14dc9c60
Amohelang Setseta <georgesetseta@gmail.com>
2023-07-17 02:59:58 -0700

    print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,7 +7,6 @@
 int main(void)
 {
 int i, j, k;
-int first, second, third;
 /* Iterate through all possible combinations of three digits*/
 for (int i = 0; i <= 7; i++)
 {
@@ -16,31 +15,17 @@ for (int j = i + 1; j <= 8; j++)
 for (int k = j + 1; k <= 9; k++)
 {
 /* Check if the combination has distinct digits*/
-if (i != j && i != k && j != k)
+putchar(i + '0');
+putchar(j + '0');
+putchar(j + '0');
+if (i != 7 || j != 8 || k != 9)
 {
-first = '0' + i;
-second = '0' + j;
-third = '0' + k;
-
-putchar(first);
-putchar(second);
-putchar(third);
-
-if (i == 7 && j == 8 && k == 9)
-{
-putchar('\n');
-return (0);
-}
-
 putchar(',');
 putchar(' ');
-
-}
-
 }
 }
-
 }
-
+}
+putchar('\n');
 return (0);
 }
