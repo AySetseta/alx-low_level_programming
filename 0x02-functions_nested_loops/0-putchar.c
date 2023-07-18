11c74445f1ea1932cf03fe6ac69c5e122de49592
Amohelang Setseta <georgesetseta@gmail.com>
2023-07-18 07:50:05 -0700

    0-putchar print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+const char message[] = "_putchar\n";
+write(1, message, sizeof(message) - 1);
+return (0);
+}
