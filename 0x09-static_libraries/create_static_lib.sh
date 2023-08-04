412c308fc8caba180242142d4a86b886368621b6
Amohelang Setseta <georgesetseta@gmail.com>
2023-08-04 13:01:33 -0700

    create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-gcc -c ./*.c
-ar -rc liball.a ./*.o
+
+# Create a variable to store the object files
+obj_files=""
+
+# Loop through all the .c files in the current directory
+for file in *.c; do
+    # Compile the .c file into an object file
+    gcc -c "$file" -o "${file%.c}.o"
+    # Add the object file to the list of object files
+    obj_files+="${file%.c}.o "
+done
+
+# Create the static library from the object files
+ar rcs liball.a $obj_files
